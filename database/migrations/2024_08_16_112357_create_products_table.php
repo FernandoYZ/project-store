b811050c046b11890293fca6dd83b5affff5d743
FernandoYZ <fernandozavala266@gmail.com>
2024-08-17 10:26:04 -0500

    [database]: updated columns table

--- database/migrations/2024_08_16_112357_create_products_table.php
+++ database/migrations/2024_08_16_112357_create_products_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('unit_measurement')->nullable();
             $table->string('image')->nullable();
             $table->string('ubication')->nullable();
-            $table->integer('status')->default(1);
+            $table->boolean('status')->default(1);
             $table->timestamps();
         });
     }
